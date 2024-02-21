ef40061ca72c4c59a6b5f7cd363bb18042a46ab9
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-21 14:52:00 +0100

    Fixed version error

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,14 @@
 fx_version 'adamant'
 games { 'gta5' }
 
-version '1.0.1'
+version '1.0.2'
 author 'HenkW'
 description "Simple Notification Script"
 
 ui_page "html/index.html"
 
 client_script "cl_notify.lua"
+server_script "version.lua"
 
 export "SetQueueMax"
 export "SendNotification"
